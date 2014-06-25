cd7a210958c7017b50a552dbf3732bacd7e846a4
Sérgio Ramos <mail@sergioramos.me>
2014-06-25 18:56:52 +0100

    no Streams1

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,8 @@
-var combine = require('stream-combiner');
 var split = require('split2');
 var through = require('through2');
 
 module.exports = function(){
-  var JSONParse = through.obj(parseJSON);
-  var stream =  combine(split(), JSONParse);
+  var stream = split().pipe(through.obj(parseJSON));
 
   function parseJSON(chunk, enc, fn){
     if(!chunk) return fn();
